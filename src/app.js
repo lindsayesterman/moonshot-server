36c72177ab4f4c2068cb6bbe9985e5316c8438a8
Lindsay Esterman <lindsayesterman@gmail.com>
2020-12-30 14:13:21 -0500

    prepare for production

--- src/app.js
+++ src/app.js
@@ -17,11 +17,6 @@ app.use(helmet())
 app.use(cors())
 app.use("/api/projects", projectsRouter)
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
-
-
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
